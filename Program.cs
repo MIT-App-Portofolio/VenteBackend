549e70e9c777312b80baa7fa3418fd7fb7b78c60
TheAlexDev23 <thealexdev23@gmail.com>
2024-12-21 16:36:17 +0100

    Secure swagger under dev env

--- Program.cs
+++ Program.cs
@@ -23,22 +23,21 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Setup aspnet core identity system with ApplicationUser and ApplicationDbContext
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    c.RoutePrefix = string.Empty;
-});
 
 app.UseHttpsRedirection();
 
