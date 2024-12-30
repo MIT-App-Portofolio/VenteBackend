47b1683220a6c7d1b6e9479de1d84fd4f8fd2217
TheAlexDev23 <thealexdev23@gmail.com>
2024-12-30 12:32:45 +0100

    Add a logout page

---
+++ Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Server.Data;
+
+namespace Server.Pages.Account;
+
+public class LogoutModel(SignInManager<ApplicationUser> signInManager) : PageModel
+{
+    public void OnGet()
+    { }
+    
+    public async Task<IActionResult> OnPostAsync()
+    {
+        await signInManager.SignOutAsync();
+        return RedirectToPage("/Index");
+    }
+}
