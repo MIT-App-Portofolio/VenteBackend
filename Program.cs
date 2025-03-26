42da921d67a603826a905963420674988ab1f559
TheAlexDev23 <thealexdev23@gmail.com>
2025-03-26 08:57:40 +0100

    I don't know why hsts was disabled

--- Program.cs
+++ Program.cs
@@ -239,7 +239,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    // app.UseHsts();
+    app.UseHsts();
 }
 
 if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Sandbox"))
