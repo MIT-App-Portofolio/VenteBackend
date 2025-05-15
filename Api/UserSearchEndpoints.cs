ce9423e1831b005cb95a8cbf0f1e1bb359928cb8
TheAlexDev23 <thealexdev23@gmail.com>
2025-05-15 14:53:10 +0200

    Add a different search endpoint for friend priority

--- Api/UserSearchEndpoints.cs
+++ Api/UserSearchEndpoints.cs
@@ -40,5 +40,39 @@ public class UserSearchEndpoints
 
                 return Results.Ok(users);
             });
+        
+        app.MapGet("/api/user_search_friends",
+            [JwtAuthorize] async (string q, HttpContext context, UserManager<ApplicationUser> userManager, IProfilePictureService pfpService) =>
+            {
+                var user = await userManager.Users.FirstOrDefaultAsync(u => u.UserName == context.User.Identity.Name);
+                if (user == null) return Results.Unauthorized();
+                
+                var qNorm = q.ToLower();
+                var users = await userManager.Users
+                    .Select(u => new
+                    {
+                        u.Name,
+                        u.UserName,
+                        u.HasPfp,
+                        u.PfpVersion,
+                        u.Blocked,
+                        u.Friends
+                    })
+                    .Where(u => u.UserName != user.UserName)
+                    .Where(u => !u.Blocked.Contains(user.UserName))
+                    .Where(u =>
+                        u.UserName.ToLower().Contains(qNorm) ||
+                        (u.Name != null &&
+                         u.Name.ToLower().Contains(qNorm)))
+                    .OrderByDescending(u => u.Friends.Contains(user.UserName))
+                    .Select(u => new UserSearchDto
+                    {
+                        Username = u.UserName,
+                        PfpUrl = u.HasPfp ? pfpService.GetDownloadUrl(u.UserName, u.PfpVersion) : pfpService.GetFallbackUrl()
+                    })
+                    .ToListAsync();
+
+                return Results.Ok(users);
+            });
     }
 }
