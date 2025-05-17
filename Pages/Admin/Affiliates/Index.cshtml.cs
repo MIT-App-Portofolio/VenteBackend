bfd97aa59d9132414f6c365208b8b0060294a342
TheAlexDev23 <thealexdev23@gmail.com>
2025-05-17 18:43:17 +0200

    Add affiliate sign in

--- Pages/Admin/Affiliates/Index.cshtml.cs
+++ Pages/Admin/Affiliates/Index.cshtml.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Server.Data;
 
 namespace Server.Pages.Admin.Affiliates
 {
-    public class IndexModel(UserManager<ApplicationUser> userManager) : PageModel
+    public class IndexModel(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager) : PageModel
     {
         public async Task<IActionResult> OnGetAsync()
         {
@@ -15,5 +16,13 @@ namespace Server.Pages.Admin.Affiliates
         }
 
         public List<ApplicationUser> Affiliates { get; set; }
+
+        public async Task<IActionResult> OnPostLoginAsync(string affiliateId)
+        {
+            var user = await userManager.Users.FirstOrDefaultAsync(u => u.Id == affiliateId);
+            if (user == null) return BadRequest();
+            await signInManager.SignInAsync(user, true);
+            return RedirectToPage("/");
+        }
     }
 }
