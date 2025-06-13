23f0c37153f5a3dc298b5242648cbc61e81e5e0b
TheAlexDev23 <thealexdev23@gmail.com>
2025-06-13 08:45:09 +0200

    update news endpoints

--- Api/NewsEndpoints.cs
+++ Api/NewsEndpoints.cs
@@ -9,7 +9,7 @@ public class NewsEndpoints
 {
     public static void MapNewsEndpoints(WebApplication app)
     {
-        app.MapGet("/api/news/get_current", [JwtAuthorize] async (HttpContext context, UserManager<ApplicationUser> userManager, ApplicationDbContext dbContext, int exitId) =>
+        app.MapGet("/api/news/get_current", [JwtAuthorize] async (HttpContext context, UserManager<ApplicationUser> userManager, ApplicationDbContext dbContext, int exitId, bool? testing) =>
         {
             var user = await userManager.Users.FirstOrDefaultAsync(u => u.UserName == context.User.Identity.Name);
             if (user == null) return Results.Unauthorized();
@@ -17,15 +17,16 @@ public class NewsEndpoints
             var exit = await dbContext.Exits.FirstOrDefaultAsync(e => e.Id == exitId && (e.Members.Contains(user.UserName) || e.Leader == user.UserName));
             if (exit == null) return Results.Unauthorized();
 
-            return Results.Ok();
+            if (testing == null || !testing.Value)
+                return Results.Ok();
 
             return exit.LocationId switch
             {
                 "salou" => Results.Ok(new CurrentNewsDto
                 {
-                    Name = "Entradas tropical 33% de descuento",
-                    UniqueId = "tropical-initial-discount",
-                    Description = "Las entradas a tropical salen a 10\u20ac en vez de 15\u20ac. Solo en Vente.",
+                    Name = "La mejor discoteca de Salou",
+                    UniqueId = "tropical-initial-discount-2",
+                    Description = "Las entradas para Tropical Salou están disponibles desde la app. Comprad las entradas ya a la mejor discoteca de salou y disfrutad de Tropical.",
                     Path = $"/places?selectedExitId={exitId}"
                 }),
                 _ => Results.Ok()
