9b9ed6cf1bbfaf090d90798e9233752c458e8950
TheAlexDev23 <thealexdev23@gmail.com>
2025-06-13 12:12:42 +0200

    Update news

--- Api/NewsEndpoints.cs
+++ Api/NewsEndpoints.cs
@@ -17,16 +17,13 @@ public class NewsEndpoints
             var exit = await dbContext.Exits.FirstOrDefaultAsync(e => e.Id == exitId && (e.Members.Contains(user.UserName) || e.Leader == user.UserName));
             if (exit == null) return Results.Unauthorized();
 
-            if (testing == null || !testing.Value)
-                return Results.Ok();
-
             return exit.LocationId switch
             {
                 "salou" => Results.Ok(new CurrentNewsDto
                 {
                     Name = "La mejor discoteca de Salou",
                     UniqueId = "tropical-initial-discount-2",
-                    Description = "Las entradas para Tropical Salou están disponibles desde la app. Comprad las entradas ya a la mejor discoteca de salou y disfrutad de Tropical.",
+                    Description = "Las entradas para Tropical Salou están disponibles desde la app. Comprad las entradas ya a la mejor discoteca de Salou y disfrutad de Tropical.",
                     Path = $"/places?selectedExitId={exitId}"
                 }),
                 _ => Results.Ok()
