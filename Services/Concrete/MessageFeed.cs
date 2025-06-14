e6ae679065ea9e50a1c122412531581ab7ae1c38
TheAlexDev23 <thealexdev23@gmail.com>
2025-06-14 12:34:56 +0200

    update message feed styling to contain individual chats

--- Services/Concrete/MessageFeed.cs
+++ Services/Concrete/MessageFeed.cs
@@ -2,17 +2,35 @@ namespace Server.Services.Concrete;
 
 public class MessageFeed
 {
-    private readonly List<string> _messages = [];
+    private readonly Dictionary<(string,string), List<string>> _messages = [];
     public void RegisterMessage(string s, string r, string m)
     {
+        var pair = (s, r);
+        if (s.CompareTo(r) < 0)
+        {
+            pair = (r, s);
+        }
+
+        var entry = $"{s}: {m}";
         lock (_messages)
         {
-            _messages.Add($"{s}->{r}: {m}");
+            var nl = new List<string> { entry };
+
+            if (!_messages.TryAdd(pair, nl))
+            {
+                _messages[pair].Add(entry);
+            }
         }
     }
 
-    public string GetMessages()
+    public List<(string, string)> GetMessages()
     {
-        return string.Join("\n", _messages);
+        lock (_messages)
+        {
+            return _messages
+                .OrderByDescending(kv => kv.Value.Count)
+                .Select(kv => ($"{kv.Key.Item1} and {kv.Key.Item2}", string.Join("\n", kv.Value)))
+                .ToList();
+        }
     }
 }
